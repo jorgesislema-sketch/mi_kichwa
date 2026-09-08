84886e2f4689fff37fef882e77cdbc24423ed2cf
jorgesislema-sketch <jorgesislema@gmail.com>
2026-09-08 13:57:29 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'providers/kichwa_provider.dart';
 import 'screens/home/home_screen.dart';
 import 'screens/grammar/grammar_intro_screen.dart';
 import 'screens/vocabulary/vocabulary_screen.dart';
@@ -7,9 +9,15 @@ import 'screens/selection/word_selection_screen.dart';
 import 'screens/matching/word_match_game_screen.dart';
 import 'screens/stories/stories_screen.dart';
 import 'screens/audios/audio_lessons_screen.dart';
+import 'screens/favorites/favorites_screen.dart';
 
 void main() {
-  runApp(const KichwaApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => KichwaProvider(),
+      child: const KichwaApp(),
+    ),
+  );
 }
 
 class KichwaApp extends StatelessWidget {
@@ -33,6 +41,7 @@ class KichwaApp extends StatelessWidget {
         '/match_game': (context) => const WordMatchGameScreen(),
         '/stories': (context) => const StoriesScreen(),
         '/audios': (context) => const AudioLessonsScreen(),
+        '/favorites': (context) => const FavoritesScreen(),
       },
     );
   }
